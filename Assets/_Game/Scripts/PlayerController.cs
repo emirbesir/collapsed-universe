5c50d29833d956d4479a1127fc7bd059f0952e23
emirbesir <emirbesirx@gmail.com>
2025-05-03 01:12:15 +0300

    feat: add player jump

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Movement Settings")]
     [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _jumpForce = 5f;
+
+    [Header("Ground Check Settings")]
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundLayer;
 
     private Rigidbody2D _rb;
-    private Vector2 _movementInput;
+    private float _movementInputX;
 
     private void Awake()
     {
@@ -15,7 +21,12 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        _movementInput = GetNormalizedMoveInput();
+        _movementInputX = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
@@ -23,15 +34,21 @@ public class PlayerController : MonoBehaviour
         Move();
     }
     
-    // Get normalized input from the player
-    private Vector2 GetNormalizedMoveInput()
-    {
-        return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-    }
-    
     // Move the player based on input
     private void Move()
     {
-        _rb.MovePosition(_rb.position + _moveSpeed * Time.deltaTime * _movementInput);
+        _rb.linearVelocity = new Vector2(_movementInputX * _moveSpeed, _rb.linearVelocity.y);
+    }
+
+    // Jump the player
+    private void Jump()
+    {
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _jumpForce); 
+    }
+
+    // Check if the player is grounded
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.1f, _groundLayer) != null;
     }
 }
